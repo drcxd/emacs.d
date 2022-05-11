5c8524a3ba975acae9c45c9466269bb822a6a49c
drcxd <drcxd@sina.com>
2022-05-11 16:27:33 +0800

    Disable doom modeline icon

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -4,6 +4,7 @@
 (require-package 'doom-modeline)
 (setq doom-modeline-buffer-file-name-style 'buffer-name)
 (setq doom-modeline-height 20)
+(setq doom-modeline-icon nil)
 (display-time)
 (column-number-mode)
 (doom-modeline-mode 1)

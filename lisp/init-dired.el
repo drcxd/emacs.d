242212a3db5518a9891d4ed62839b39c988f9ff8
drcxd <drcxd@sina.com>
2022-05-13 10:40:51 +0800

    Dired hide detail

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -2,5 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 (setq dired-kill-when-opening-new-dired-buffer t)
+(defun dired-mode-setup ()
+  "Setup dired."
+  (dired-hide-details-mode 1))
+(add-hook 'dired-mode-hook 'dired-mode-setup)
 (provide 'init-dired)
 ;;; init-dired.el ends here

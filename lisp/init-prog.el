5ce7629c4796c1a813c55b20fe75aa8312852a4b
drcxd <drcxd@sina.com>
2022-05-17 11:53:13 +0800

    Add diff-hl setup in case git-gutter does not work

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -40,7 +40,12 @@
 
 ;; git-gutter
 (require-package 'git-gutter)
+(require 'git-gutter)
 (add-hook 'prog-mode-hook 'git-gutter-mode)
+;; or diff-hl if git-gutter does not work
+(require-package 'diff-hl)
+(require 'diff-hl)
+(add-hook 'prog-mode-hook 'diff-hl-mode)
 
 ;; doxygen documentation style
 (setq-default c-doc-comment-style

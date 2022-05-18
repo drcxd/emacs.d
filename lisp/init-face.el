ff87aa9b7f5f9535a4c23501abf4da5f95b6d38b
drcxd <drcxd@sina.com>
2022-05-18 16:21:29 +0800

    Switch to modus themes

--- lisp/init-face.el
+++ lisp/init-face.el
@@ -2,16 +2,19 @@
 ;;; Commentary:
 ;;; Code:
 
+(set-face-attribute 'default nil :font "Cascadia Code-16")
+(set-face-attribute 'fixed-pitch nil :font "Cascadia Code-16")
+
 ;; italic
 (set-face-attribute 'italic nil
                     :slant 'italic
                     :underline nil)
 
 ;; markdown
-(require-package 'markdown-mode)
-(require 'markdown-mode)
-(set-face-attribute 'markdown-code-face nil
-                    :family "Cascadia Code")
+;; (require-package 'markdown-mode)
+;; (require 'markdown-mode)
+;; (set-face-attribute 'markdown-code-face nil
+;;                     :family "Cascadia Code")
 
 (provide 'init-face)
 ;;; init-face.el ends here
--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -4,13 +4,15 @@
 
 (require-package 'doom-themes)
 (require-package 'gruvbox-theme)
+(require-package 'modus-themes)
 
 ;; Don't prompt to confirm theme safety. This avoids problems with
 ;; first-time startup on Emacs > 26.3.
 (setq custom-safe-themes t)
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(gruvbox))
+;; (setq-default custom-enabled-themes '(gruvbox))
+(setq-default custom-enabled-themes '(modus-vivendi))
 
 ;; Ensure that themes will be applied even if they have not been
 ;; customized

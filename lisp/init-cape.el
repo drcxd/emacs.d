3f9a7b75cca72b93a6d889996f767581bcdc4fa4
drcxd <drcxd@sina.com>
2022-05-27 00:06:55 +0800

    Add cape setting

--- lisp/init-cape.el
+++ lisp/init-cape.el
@@ -7,6 +7,8 @@
 (require 'company-clang)
 (add-to-list 'completion-at-point-functions
              (cape-company-to-capf #'company-clang))
+(add-to-list 'completion-at-point-functions #'cape-file)
+(add-to-list 'completion-at-point-functions #'cape-dabbrev)
 
 (provide 'init-cape)
 ;;; init-cape.el ends here

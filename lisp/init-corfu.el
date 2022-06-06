120ed1091741f5b9537857fa184757fa62c86e46
drcxd <drcxd@sina.com>
2022-06-06 20:14:46 +0800

    Enable corfu in minibuffer

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -8,5 +8,12 @@
       corfu-quit-at-boundary nil
       corfu-quit-no-match t
       corfu-preview-current nil)
+
+(defun corfu-enable-in-minibuffer ()
+  "Enable Corfu in the minibuffer if `completion-at-point' is bound."
+  (when (where-is-internal #'completion-at-point (list (current-local-map)))
+    (corfu-mode 1)))
+(add-hook 'minibuffer-setup-hook #'corfu-enable-in-minibuffer)
+
 (provide 'init-corfu)
 ;;; init-corfu.el ends here

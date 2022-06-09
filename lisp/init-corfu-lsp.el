44d945e63097868792de618b7a3734a62c800eb3
drcxd <drcxd@sina.com>
2022-06-09 09:59:15 +0800

    Handle corfu-lsp completion halt issue

--- lisp/init-corfu-lsp.el
+++ lisp/init-corfu-lsp.el
@@ -3,12 +3,14 @@
 ;;; Code:
 (require-package 'corfu)
 (require-package 'lsp-mode)
+(require-package 'cape)
 
 ;; corfu completion setup
 (setq lsp-completion-provider :none)
 (defun corfu-lsp-setup ()
   (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
-        '(orderless)))
+        '(orderless))
+  (advice-add #'lsp-completion-at-point :around #'cape-wrap-noninterruptible))
 (add-hook 'lsp-completion-mode-hook #'corfu-lsp-setup)
 
 (provide 'init-corfu-lsp)

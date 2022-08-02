5575123c48894038f3994d5eab88cd7d1e48cb11
drcxd <drcxd@sina.com>
2022-08-02 19:50:13 +0800

    Handle Chinese characters in consult-ripgrep

--- lisp/init-consult.el
+++ lisp/init-consult.el
@@ -4,5 +4,10 @@
 (require-package 'consult)
 (setq xref-show-xrefs-function #'consult-xref
       xref-show-definitions-function #'consult-xref)
+
+(if (eq system-type 'ms-dos)
+    (add-to-list 'process-coding-system-alist
+                 '("[rR][gG]" . (utf-8 . gbk-dos))))
+
 (provide 'init-consult)
 ;;; init-consult.el ends here

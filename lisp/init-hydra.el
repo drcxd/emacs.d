8f98c2a0468af2e3a965ca5f4c6bbfc612a61c3b
drcxd <drcxd@sina.com>
2022-08-13 10:46:28 +0800

    Update hydra to bind to vanilla commands

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -4,17 +4,10 @@
 (require-package 'hydra)
 (require 'hydra)
 
-(defhydra hydra-org-roam (:exit t)
-  "org-roam"
-  ("f" org-roam-node-find "node-find")
-  ("i" org-roam-node-insert "node-insert")
-  ("a" org-roam-tag-add "tag-add")
-  ("r" org-roam-tag-remove "tag-remove"))
-
 (defhydra hydra-navigation ()
   "hydra-navigation"
-  ("j" evil-next-line "next-line")
-  ("k" evil-previous-line "previous-line")
+  ("j" next-line "next-line")
+  ("k" previous-line "previous-line")
   ("f" scroll-up-command "page-down")
   ("b" scroll-down-command "page-up")
   ("q" nil "quit"))

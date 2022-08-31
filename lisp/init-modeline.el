7dc2ea6972ffb0d66f23a14d8ad5f813b55ae1b1
drcxd <drcxd@sina.com>
2022-08-31 16:24:57 +0800

    Minor adjustment

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -1,12 +1,16 @@
 ;;; init-modeline --- summary
 ;;; Commentary:
 ;;; Code:
+
 (require-package 'doom-modeline)
+
 (setq doom-modeline-buffer-file-name-style 'buffer-name)
 (setq doom-modeline-icon nil)
 (setq display-time-24hr-format t)
+(doom-modeline-mode 1)
+
 (display-time)
 (column-number-mode)
-(doom-modeline-mode 1)
+
 (provide 'init-modeline)
 ;;; init-modeline.el ends here

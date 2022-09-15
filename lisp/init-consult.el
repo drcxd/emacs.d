5470cac3fabc693c0dc1701311569598fbf3f1a8
drcxd <drcxd@sina.com>
2022-09-15 15:09:20 +0800

    Add consult fd

--- lisp/init-consult.el
+++ lisp/init-consult.el
@@ -18,5 +18,31 @@
 (global-set-key (kbd "C-c r") 'consult-ripgrep)
 ;; (global-set-key (kbd "C-c f") 'consult-recent-file)
 
+(defvar consult--fd-command nil)
+(defun consult--fd-builder (input)
+  (unless consult--fd-command
+    (setq consult--fd-command
+          (if (eq 0 (call-process-shell-command "fdfind"))
+              "fdfind"
+            "fd")))
+  (pcase-let* ((`(,arg . ,opts) (consult--command-split input))
+               (`(,re . ,hl) (funcall consult--regexp-compiler
+                                      arg 'extended t)))
+    (when re
+      (list :command (append
+                      (list consult--fd-command
+                            "--color=never" "--full-path"
+                            (consult--join-regexps re 'extended))
+                      opts)
+            :highlight hl))))
+
+(defun consult-fd (&optional dir initial)
+  (interactive "P")
+  (let* ((prompt-dir (consult--directory-prompt "Fd" dir))
+         (default-directory (cdr prompt-dir)))
+    (find-file (consult--find (car prompt-dir) #'consult--fd-builder initial))))
+
+(global-set-key (kbd "C-c f") 'consult-fd)
+
 (provide 'init-consult)
 ;;; init-consult.el ends here

bc53aea57116ae0160d852b659da0a89e7a93ead
drcxd <drcxd@sina.com>
2022-09-20 11:45:16 +0800

    Remove org-roam and add more denote key bindings

--- lisp/init-note.el
+++ lisp/init-note.el
@@ -1,26 +1,26 @@
 ;;; init-note --- summary
 ;;; Commentary:
 ;;; Code:
-(require-package 'org-roam)
-(with-eval-after-load 'org-roam
-  (org-roam-db-autosync-mode)
-  (setq org-roam-node-display-template
-        (concat "${title} "
-                (propertize "${tags:*}" 'face 'org-tag))))
+;; (require-package 'org-roam)
+;; (with-eval-after-load 'org-roam
+;;   (org-roam-db-autosync-mode)
+;;   (setq org-roam-node-display-template
+;;         (concat "${title} "
+;;                 (propertize "${tags:*}" 'face 'org-tag))))
 
-(require-package 'org-roam-ui)
-(with-eval-after-load 'org-roam-ui
-  (setq org-roam-ui-sync-theme nil
-        org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start nil))
+;; (require-package 'org-roam-ui)
+;; (with-eval-after-load 'org-roam-ui
+;;   (setq org-roam-ui-sync-theme nil
+;;         org-roam-ui-follow t
+;;         org-roam-ui-update-on-save t
+;;         org-roam-ui-open-on-start nil))
 
-(global-set-key (kbd "C-c ni") 'org-roam-node-insert)
-(global-set-key (kbd "C-c nf") 'org-roam-node-find)
-(global-set-key (kbd "C-c nt") 'org-roam-tag-add)
-(global-set-key (kbd "C-c nT") 'org-roam-tag-remove)
-(global-set-key (kbd "C-c nb") 'org-roam-buffer-display-dedicated)
-(global-set-key (kbd "C-c nB") 'org-roam-buffer-toggle)
+;; (global-set-key (kbd "C-c ni") 'org-roam-node-insert)
+;; (global-set-key (kbd "C-c nf") 'org-roam-node-find)
+;; (global-set-key (kbd "C-c nt") 'org-roam-tag-add)
+;; (global-set-key (kbd "C-c nT") 'org-roam-tag-remove)
+;; (global-set-key (kbd "C-c nb") 'org-roam-buffer-display-dedicated)
+;; (global-set-key (kbd "C-c nB") 'org-roam-buffer-toggle)
 
 (defvar my-denote-directory "~/notes/")
 (require-package 'denote)
@@ -42,6 +42,8 @@
 (global-set-key (kbd "C-c nl") 'denote-link)
 (global-set-key (kbd "C-c nL") 'denote-link-backlinks)
 (global-set-key (kbd "C-c no") 'denote-link-find-file)
+(global-set-key (kbd "C-c nr") 'denote-rename-file)
+(global-set-key (kbd "C-c nR") 'denote-rename-file-using-front-matter)
 (global-set-key (kbd "C-c nj") 'my-denote-journal)
 
 (require-package 'consult-notes)

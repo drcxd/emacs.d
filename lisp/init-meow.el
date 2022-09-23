99e3f6f8f186377a5472a9f1039977fc6779b1d6
drcxd <drcxd@sina.com>
2022-09-23 15:04:02 +0800

    Chane meow-keypad short cut

--- lisp/init-meow.el
+++ lisp/init-meow.el
@@ -101,7 +101,7 @@
 (global-set-key (kbd "C-c w2") 'split-window-vertically)
 (global-set-key (kbd "C-c w3") 'split-window-horizontally)
 (global-set-key (kbd "C-c w0") 'delete-window)
-(global-set-key (kbd "<f7>") 'meow-keypad)
+(global-set-key (kbd "C-.") 'meow-keypad)
 
 (defvar my-lsp-map
   (let ((map (make-sparse-keymap)))

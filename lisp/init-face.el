eb4120a75bc70294c58ef8fbb9a0fae914271ac5
drcxd <drcxd@sina.com>
2022-09-28 09:40:10 +0800

    fixed-pitch and variable-pitch face scale with their parent

--- lisp/init-face.el
+++ lisp/init-face.el
@@ -6,8 +6,12 @@
 (set-fontset-font "fontset-default" 'han "Microsoft Yahei")
 
 (set-face-attribute 'default nil :font "Cascadia Code-16")
-(set-face-attribute 'fixed-pitch nil :font "Cascadia Code")
-(set-face-attribute 'variable-pitch nil :font "FiraGo")
+(set-face-attribute 'fixed-pitch nil
+                    :font "Cascadia Code"
+                    :height 1.0)
+(set-face-attribute 'variable-pitch nil
+                    :font "FiraGo"
+                    :height 1.0)
 
 ;; italic
 (set-face-attribute 'italic nil

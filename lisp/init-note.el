459a8ff20ee717b18ca0d6af88a536f4434fc563
drcxd <drcxd@sina.com>
2022-10-08 22:57:12 +0800

    Upgrade denote to 1.0

--- lisp/init-note.el
+++ lisp/init-note.el
@@ -38,10 +38,58 @@
           "text"
           (denote--subdirs-prompt)))
 
-(global-set-key (kbd "C-c nd") 'denote)
-(global-set-key (kbd "C-c ns") 'denote-subdirectory)
-(global-set-key (kbd "C-c nt") 'denote-type)
-(global-set-key (kbd "C-c nl") 'denote-link)
+;;;###autoload
+(defun denote-open-or-create-with-date ()
+  "Invoke `denote-open-or-create' but also prompt for date.
+
+The date can be in YEAR-MONTH-DAY notation like 2022-06-30 or
+that plus the time: 2022-06-16 14:30.  When the user option
+`denote-date-prompt-use-org-read-date' is non-nil, the date
+prompt uses the more powerful Org+calendar system.
+
+This is the equivalent to calling `denote-open-or-create' when
+`denote-prompts' is set to \\='(date title keywords)."
+  (declare (interactive-only t))
+  (interactive)
+  (let ((denote-prompts '(date title keywords)))
+    (call-interactively #'denote-open-or-create)))
+
+;;;###autoload
+(defun denote-open-or-create-with-type ()
+  "Invoke `denote-open-or-create' but also prompt for file type.
+This is the equivalent to calling `denote-open-or-create' when
+`denote-prompts' is set to \\='(type title keywords)."
+  (declare (interactive-only t))
+  (interactive)
+  (let ((denote-prompts '(type title keywords)))
+    (call-interactively #'denote-open-or-create)))
+
+;;;###autoload
+(defun denote-open-or-create-with-subdirectory ()
+  "Invoke `denote-open-or-create' but also prompt for subdirectory.
+This is the equivalent to calling `denote-open-or-create' when
+`denote-prompts' is set to \\='(subdirectory title keywords)."
+  (declare (interactive-only t))
+  (interactive)
+  (let ((denote-prompts '(subdirectory title keywords)))
+    (call-interactively #'denote-open-or-create)))
+
+;;;###autoload
+(defun denote-open-or-create-with-template ()
+  "Invoke `denote-open-or-create' but also prompt for template.
+This is the equivalent to calling `denote-open-or-create' when
+`denote-prompts' is set to \\='(template title keywords).
+
+For templates, refer to `denote-templates'."
+  (declare (interactive-only t))
+  (interactive)
+  (let ((denote-prompts '(template title keywords)))
+    (call-interactively #'denote-open-or-create)))
+
+(global-set-key (kbd "C-c nd") 'denote-open-or-create)
+(global-set-key (kbd "C-c ns") 'denote-open-or-create-with-subdirectory)
+(global-set-key (kbd "C-c nt") 'denote-open-or-create-with-type)
+(global-set-key (kbd "C-c nl") 'denote-link-or-create)
 (global-set-key (kbd "C-c nL") 'denote-link-backlinks)
 (global-set-key (kbd "C-c no") 'denote-link-find-file)
 (global-set-key (kbd "C-c nr") 'denote-rename-file)

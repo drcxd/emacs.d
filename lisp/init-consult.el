f17b5cf984d695664255faa9465eb0b94f4fe415
drcxd <drcxd@sina.com>
2022-10-27 20:16:59 +0800

    Add a convenient function call consult-ripgrep with prefix

--- lisp/init-consult.el
+++ lisp/init-consult.el
@@ -13,10 +13,16 @@
       ;; https://github.com/minad/consult/issues/601
       (setq consult-find-args (concat find-program " . "))))
 
+(defun consult-ripgrep-with-prefix ()
+  (interactive)
+  (let ((current-prefix-arg '(4)))
+    (call-interactively 'consult-ripgrep)))
+
 (setq consult-preview-excluded-files '(".*org"))
 (define-key (current-global-map) [remap switch-to-buffer] 'consult-buffer)
 (global-set-key (kbd "C-c i") 'consult-line)
-(global-set-key (kbd "C-c r") 'consult-ripgrep)
+(global-set-key (kbd "C-c r") 'consult-ripgrep-with-prefix)
+(global-set-key (kbd "C-c R") 'consult-ripgrep)
 (global-set-key (kbd "C-c I") 'consult-imenu)
 ;; (global-set-key (kbd "C-c f") 'consult-recent-file)
 

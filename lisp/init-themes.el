f0c4c21ddaabef4ead6e3c032a9509e1bbb444fc
drcxd <drcxd@sina.com>
2022-11-16 14:26:33 +0800

    Theme changes

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -54,7 +54,7 @@ This function is added to the `ef-themes-post-load-hook'."
 
 (with-eval-after-load 'ef-themes
   (setq
-   ef-themes-to-toggle '(ef-day ef-night)
+   ef-themes-to-toggle '(ef-light ef-dark)
    ef-themes-mixed-fonts t
    ef-themes-variable-pitch-ui t
    )
@@ -66,7 +66,7 @@ This function is added to the `ef-themes-post-load-hook'."
 (defun my-use-ef-themes ()
   (interactive)
   (mapc #'disable-theme custom-enabled-themes)
-  (ef-themes-select 'ef-day)
+  (ef-themes-select 'ef-light)
   (global-set-key (kbd "C-c tg") 'ef-themes-toggle)
   )
 
